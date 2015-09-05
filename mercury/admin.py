6ab4750a609e9016e2e85c395de04ec0d885bfe7
jonstacks13 <jonstacks13@gmail.com>
2015-09-05 03:00:59 -0500

    Change node Admin display

--- mercury/admin.py
+++ mercury/admin.py
@@ -15,9 +15,14 @@ class FileAdmin(admin.ModelAdmin):
     list_filter = ('processed', )
     readonly_fields = ('processed', )
 
+
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ('dns_name', 'ip_address', )
+
+
 admin.site.register(Application)
 admin.site.register(ApplicationPacket)
 admin.site.register(ApplicationTraffic)
-admin.site.register(Node)
+admin.site.register(Node, NodeAdmin)
 admin.site.register(PCAPFile, FileAdmin)
 admin.site.register(TransportProtocol)
